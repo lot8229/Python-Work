07e5b6ca58991ebabbd506950705d547393f8fb1
username <user@email.mail>
2020-10-05 17:56:33 +0900

    Python_Day_3

---
+++ 10.py
@@ -0,0 +1,13 @@
+if "":# "" or 0 or false 는 False로 인식됩니다
+    # print("0은 True로 변환됩니다.")
+    pass
+else:
+    print("0은 False로 변환됩니다.")
+array = [271,12349,29341294,194039049102]
+print(array[1])
+list_a=[1,2,3,4,5]
+list_b=[6,7,8,9,10]
+print(list_a+list_b)
+
+list_a.append("문자열")
+print(list_a)
