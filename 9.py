d9c69b79b72aa6d63ecb544b7eab3cf7fdfe522a
username <user@email.mail>
2020-10-05 17:29:17 +0900

    Python_Day_3

--- 9.py
+++ 9.py
@@ -2,3 +2,20 @@
 #4.2~4.5 : 사랑
 #3.5~4.2 : 수호자
 #2.8~3.5 : 일반인
+#2.3~2.8 : 일탈
+#1.75~2.3 : 오락
+#1.0~1.75 : 불가촉천민
+num = input("학점임력 : ")
+num=float(num)
+if num>4.5 : 
+    print("신")
+if num>4.2 and num<=4.5 : 
+    print("사람")
+if num>3.5 and num<=4.2 : 
+    print("사람")
+if num>2.8 and num<=3.5 : 
+    print("사람")
+if num>1.75 and num<=2.8 : 
+    print("사람")
+if num>1 and num<=1.75 : 
+    print("사람")
